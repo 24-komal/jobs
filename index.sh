0dab36e3279ef0bc3c6bab2b568121655f0ca0c0
Komal Suthar <69863007+24-komal@users.noreply.github.com>
2021-04-02 12:27:05 +0530

    Update index.sh

--- index.sh
+++ index.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 date
 cal
-sudo echo "AND now SOON IWLL BE GOING TO CONTRIBUTE TO RED HAT COMMUNTIY BY CONTRIBUTING INIT :)" >> /var/www/html/index.html 
+echo "AND now SOON IWLL BE GOING TO CONTRIBUTE TO RED HAT COMMUNTIY BY CONTRIBUTING INIT :)" >> /var/www/html/index.html 
 date
